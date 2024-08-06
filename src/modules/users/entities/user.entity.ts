d158c84ffb45eb4350e119334ade6a39494d7edd
khai123-2 <tranquangkhai9102gmail.com>
2024-08-07 00:03:12 +0700

    Add model

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,6 +1,7 @@
 import { CHAIN_TYPE } from 'src/constants';
 import { AbstractEntity } from 'src/database/abstract/abstract.entity';
-import { Column, Entity } from 'typeorm';
+import { ReservationEntity } from 'src/modules/reservations/entities/reservation.entity';
+import { Column, Entity, OneToMany } from 'typeorm';
 @Entity({ name: 'users' })
 export class UserEntity extends AbstractEntity {
   @Column({ type: 'varchar', nullable: false, unique: true })
@@ -11,4 +12,7 @@ export class UserEntity extends AbstractEntity {
 
   @Column({ type: 'enum', enum: CHAIN_TYPE, default: CHAIN_TYPE.EVM })
   chainType: CHAIN_TYPE;
+
+  @OneToMany(() => ReservationEntity, (reservation) => reservation.user)
+  reservations: ReservationEntity[];
 }
