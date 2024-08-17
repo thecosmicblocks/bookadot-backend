d8894766e98eb6722a3d1bc389e5363785b143c1
khanhdd <duykhanhheh1996@gmail.com>
2024-08-17 23:44:03 +0700

    API get list reservation and get reservation detail

--- src/modules/reservations/dtos/request.dto.ts
+++ src/modules/reservations/dtos/request.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumber, IsUUID } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsOptional, IsUUID } from 'class-validator';
 
 export class PurchaseTicketRequest {
   @ApiProperty({required: true})
@@ -15,4 +15,43 @@ export class PurchaseTicketRequest {
   @IsNotEmpty()
   @IsNumber()
   totalPrice: number;
+}
+
+export class FilterListDto {
+  @ApiProperty({required: true})
+  @IsNotEmpty()
+  @IsUUID()
+  userId: string;
+
+  @ApiProperty({required: false})
+  @IsOptional()
+  @IsUUID()
+  sessionId: string;
+}
+
+export class FilterDetailDto {
+  @ApiProperty({required: true})
+  @IsNotEmpty()
+  @IsUUID()
+  userId: string;
+
+  @ApiProperty({required: true})
+  @IsNotEmpty()
+  @IsUUID()
+  reservationId: string;
+
+  @ApiProperty({required: true})
+  @IsNotEmpty()
+  @IsUUID()
+  reservationInfoId: string;
+
+  @ApiProperty({required: false})
+  @IsOptional()
+  @IsUUID()
+  sessionId: string;
+
+  @ApiProperty({required: false})
+  @IsOptional()
+  @IsUUID()
+  ticketId: string;
 }
--- src/modules/reservations/reservation.service.ts
+++ src/modules/reservations/reservation.service.ts
@@ -4,7 +4,6 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DataSource, Repository, Like } from 'typeorm';
-import { ReservationInfoService } from '../reservation-infos/reservation-info.service';
 
 import { ReservationEntity } from './entities/reservation.entity';
 import { ReservationInfoEntity } from '../reservation-infos/entities/reservation-info.entity';
@@ -16,6 +15,75 @@ export class ReservationService {
     private reservationRepository: Repository<ReservationEntity>,
     private dataSource: DataSource,
   ) {}
+
+  async getReservations(params: any): Promise<ReservationEntity[]> {
+    const { userId, sessionId = '' } = params;
+
+    const queryBuilder = this.reservationRepository.createQueryBuilder('reservation')
+      .select([
+        'reservation.id',
+        'reservation.userId',
+        'reservation.totalPrice',
+        'reservation.createdAt'
+      ])
+      .leftJoin('reservation.reservationInfos', 'reservationInfo')
+      .leftJoin('reservationInfo.ticket', 'ticket')
+      .where('reservation.userId = :userId', { userId });
+
+      if (sessionId) {
+        queryBuilder.andWhere('reservationInfo.sessionId = :sessionId', { sessionId });
+      }
+
+      queryBuilder
+      .addSelect([
+        'reservation.id',
+        'reservationInfo.id', 'reservationInfo.createdAt',
+        'ticket.id', 'ticket.price', 'ticket.ticketType',
+      ]);
+
+    return queryBuilder.getMany();
+  }
+
+  async getReservationDetail(params: any): Promise<ReservationEntity[]> {
+    const { userId, sessionId, reservationId, reservationInfoId, ticketId } = params;
+
+    const queryBuilder = this.reservationRepository.createQueryBuilder('reservation')
+      .select([
+        'reservation.id',
+        'reservation.userId',
+        'reservation.totalPrice',
+      ])
+      .leftJoin('reservation.reservationInfos', 'reservationInfo')
+      .leftJoin('reservationInfo.session', 'session')
+      .leftJoin('session.movie', 'movie')
+      .leftJoin('reservationInfo.ticket', 'ticket')
+      .leftJoin('reservationInfo.seat', 'seat')
+      .leftJoin('seat.theatre', 'theatre')
+      .where('reservation.userId = :userId', { userId })
+      .andWhere('reservation.id = :reservationId', { reservationId })
+      .andWhere('reservationInfo.id = :reservationInfoId', { reservationInfoId });
+
+    if (sessionId) {
+      queryBuilder.andWhere('reservationInfo.sessionId = :sessionId', { sessionId });
+    }
+
+    if (ticketId) {
+      queryBuilder.andWhere('reservationInfo.ticketId = :ticketId', { ticketId });
+    }
+
+    queryBuilder
+      .addSelect([
+        'reservation.id',
+        'reservationInfo.id',
+        'session.id', 'session.movieId', 'session.startTime', 'session.endTime', 'session.format', 'session.language',
+        'movie.title', 'movie.description', 'movie.posterUrl',
+        'ticket.id', 'ticket.price', 'ticket.ticketType',
+        'seat.id', 'seat.row', 'seat.column', 'seat.coordinates', 'seat.theatreId',
+        'theatre.id', 'theatre.name', 'theatre.address', 'theatre.location'
+      ]);
+
+    return queryBuilder.getMany();
+  }
 
   async storeTicketPurchase(
     sessionId: string,
